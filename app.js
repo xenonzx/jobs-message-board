eb4876540539fa9bddbd0df52766bc2209065170
Ahmed EL-Dakhakhny <>
2020-04-07 22:11:01 +0200

    basic app,js

---
+++ app.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const app =  express();
+app.get('/', function(req, res){
+    res.send("helloworld");
+});
+app.listen(process.env.PORT || 3000, 
+	() => console.log("Server is running..."));
