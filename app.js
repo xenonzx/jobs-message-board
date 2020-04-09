7a12795cd37cd390d1e2e6453de08eaa7ccb8235
Ahmed EL-Dakhakhny <>
2020-04-09 12:48:43 +0200

    getting id passed in params

--- app.js
+++ app.js
@@ -1,7 +1,17 @@
 const express = require('express');
 const app =  express();
+const crypto = require('crypto');
 app.get('/', function(req, res){
     res.send("helloworld");
 });
+
+app.put('/message/:ID', function(req, res){
+    const id = req.params.ID;
+    res.send(crypto.createHash('sha1')
+      .update(new Date().toDateString() + id)
+      .digest('hex'));
+});
 app.listen(process.env.PORT || 3000, 
-	() => console.log("Server is running..."));
+    () => console.log("Server is running..."));
+    
+    
