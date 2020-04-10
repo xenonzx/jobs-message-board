5fba6b896eb1de21411f54a061560ba1d5533110
Ahmed EL-Dakhakhny <>
2020-04-10 22:13:03 +0200

    saving jobs in db

--- app.js
+++ app.js
@@ -2,6 +2,20 @@ const express = require('express');
 const app =  express();
 const crypto = require('crypto');
 const bodyParser = require('body-parser');
+const MongoClient = require('mongodb').MongoClient;
+// TODO get it from env var
+const mongoUrl = 'mongodb://127.0.0.1:27017'
+const dbName = 'jobs-board-db'
+let db;
+let jobsCollection;
+MongoClient.connect(mongoUrl, (err, client) => {
+    if (err) return console.log(err);
+    console.log(`Connected MongoDB: ${mongoUrl}`);
+    console.log(`Database: ${dbName}`);
+    db = client.db(dbName)
+    jobsCollection = db.collection('jobs');
+
+  })
 
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -20,6 +34,10 @@ app.put('/message/:ID', function(req, res){
 app.post('/jobs', function(req, res){
     console.log(req.body);
     let job = {};
+    // TODO server side validation
+    if (!db||!jobsCollection){
+        return console("error in creating  job in jobs collection");
+    }
     job.responsibility = req.body.responsibility
     job.position = req.body.position
     job.requirements = req.body.requirements
@@ -27,6 +45,11 @@ app.post('/jobs', function(req, res){
     job.website = req.body.website
     job.email = req.body.email
     job.about = req.body.about
+    jobsCollection.insertOne(req.body)
+    .then(result => {
+      console.log(result)
+    })
+    .catch(error => console.error(error))
     console.log(job);
     res.json(req.body);
 });
