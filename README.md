59e9ff9c25cad7ba7e95c80a415fa7db8c7667da
Ahmed EL-Dakhakhny <>
2020-04-12 16:06:13 +0200

    connecting to mongo db through env variable

---
+++ README.md
@@ -0,0 +1,4 @@
+.env file shoul contain all env variables
+list of env vars can be found at template.env
+mongo "mongodb+srv://cluster0-twfkq.mongodb.net/jobs-board-db"  --username app
+
--- app.js
+++ app.js
@@ -3,26 +3,24 @@ const app =  express();
 const crypto = require('crypto');
 const bodyParser = require('body-parser');
 const MongoClient = require('mongodb').MongoClient;
-// TODO get it from env var
-const mongoUrl = 'mongodb://127.0.0.1:27017'
-const dbName = 'jobs-board-db'
+
+require('dotenv').config();
+
+const mongoUrl =  process.env.MONGO_URL;
 let db;
+const dbName = 'jobs-board-db'
 let jobsCollection;
 
 app.set('view engine', 'ejs');
-// parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }));
-// parse application/json
 app.use(bodyParser.json());
+
 MongoClient.connect(mongoUrl, (err, client) => {
     if (err) return console.log(err);
     console.log(`Connected MongoDB: ${mongoUrl}`);
-    console.log(`Database: ${dbName}`);
-    db = client.db(dbName)
+    db = client.db(dbName);
     jobsCollection = db.collection('jobs');
-
   })
-
 
 app.get('/', function(req, res){
     res.send("helloworld");
@@ -40,7 +38,8 @@ app.get('/jobs', function(req, res){
         res.render('index.ejs', { jobs: results })
         //res.send(results);
       });
-})
+});
+
 app.post('/jobs', function(req, res){
     console.log(req.body);
     let job = {};
@@ -56,13 +55,17 @@ app.post('/jobs', function(req, res){
     job.website = req.body.website
     job.email = req.body.email
     job.about = req.body.about
-    jobsCollection.insertOne(req.body)
+    let responseMessage ;
+    jobsCollection.insertOne(job)
     .then(result => {
       console.log(result)
+      res.json(result);
     })
-    .catch(error => console.error(error))
-    console.log(job);
-    res.json(req.body);
+    .catch(error => {console.error(error)
+      res.status(500).send();
+    })
+    
+    
 });
 
 app.listen(process.env.PORT || 3000, 
